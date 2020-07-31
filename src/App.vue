009b9384dab3dba8b33734dd63ec9153319406eb
unknown <2530679391@qq.com>
2020-07-31 12:28:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+    <main-tab-bar></main-tab-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from "./components/mainTabbar/MainTabBar";
+export default {
+  name: 'App',
+  components:{
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+
+</style>
